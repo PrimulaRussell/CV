ba13b3d6ec5a95b8b1ab5192caa1c6e113863e25
primula <primularussell@gmail.com>
2021-01-11 12:57:43 -0700

    Made marquee functional

--- js/main.js
+++
@@ -1,7 +0,0 @@
-fetch('http://api.forismatic.com/api/1.0/')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response);
-    });
