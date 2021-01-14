1c7bcc62d843a0ea7c4ee85dfe38b25e08bf8541
primula <primularussell@gmail.com>
2021-01-13 22:34:47 -0700

    connected firebase to contact form

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDyyRHD83vszzsdnDDlNqS_ntZXVt2OuYE",
+    authDomain: "contact-form-745ad.firebaseapp.com",
+    projectId: "contact-form-745ad",
+    storageBucket: "contact-form-745ad.appspot.com",
+    messagingSenderId: "49546093402",
+    appId: "1:49546093402:web:357edbdd57a70721948963"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Reference contact info collections
+let contactInfo = firebase.database().ref("info");
+
+//Listen for submit event
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    //Get input values
+    let name = document.querySelector(".name").value;
+    let email = document.querySelector(".email").value;
+    let message = document.querySelector(".message").value;
+
+    saveContactInfo(name, email, message);
+}
+
+function saveContactInfo(name, email, message) {
+    let newContactInfo = contactInfo.push();
+
+    newContactInfo.set({
+
+        name: name,
+        email: email,
+        message: message,
+    });
+
+}
